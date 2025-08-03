797ed3eff01351e3be3c12e06f6a15b6e7a7b207
KamalakshmiPriya-Allu <allukamalakshmipriyaa@gmail.com>
2025-08-03 11:10:45 +0530

    Create day01.py
    Day 1: Created a python band name generator in vs code

---
+++ Day-01/day01.py
@@ -0,0 +1,6 @@
+#Day1 of code 
+#Building a Band Name Generator
+print("Welcome to the Band Name Generator")
+city = input("What's the name of the city you grew up in?")
+pet = input("What's your pet name?")
+print("Your band name can be:" + city + pet)
